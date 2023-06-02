caf9dee76707e2f736af361386a263f0edf71aba
lavinigam <lavinigam@google.com>
2023-06-02 11:13:43 +0530

    deleted unused files

--- demo/streamlit/hello-world-chat/app.py
+++ demo/streamlit/hello-world-chat/app.py
@@ -3,7 +3,7 @@ from vertexai.preview.language_models import ChatModel, InputOutputTextPair, Cha
 import vertexai
 from streamlit_chat import message
 
-PROJECT_ID = "cloud-llm-preview1"
+PROJECT_ID = ""
 vertexai.init(project=PROJECT_ID, location="us-central1")
 
 def create_session(temperature=0.2,
@@ -90,99 +90,4 @@ with tab1:
                 # st.write(i)
                 message(st.session_state['past'][i], is_user=True, key=str(i) + '_user', avatar_style='big-smile')
                 message(st.session_state["generated"][i], key=str(i), avatar_style='bottts')
-                
-
-
-
-
-
-
-# experimental 
-# user_input = get_text()
-
-# if user_input:
-#     output = query({
-#         "inputs": {
-#             "past_user_inputs": st.session_state.past,
-#             "generated_responses": st.session_state.generated,
-#             "text": user_input,
-#         },"parameters": {"repetition_penalty": 1.33},
-#     })
-
-#     st.session_state.past.append(user_input)
-#     st.session_state.generated.append(output["generated_text"])
-
-# if st.session_state['generated']:
-
-#     for i in range(len(st.session_state['generated'])-1, -1, -1):
-#         message(st.session_state["generated"][i], key=str(i))
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-# generation_model = TextGenerationModel.from_pretrained("text-bison@001")
-
-
-# st.title("PaLM 2 Demo - Question Answering with multiple, large documents")
-
-# st.sidebar.button("Method 1")
-# prompt = "What is a large language model?"
-# response = generation_model.predict(prompt=prompt)
-# st.write(response.text)
-
-# st.write(st.session_state["generated"])
-# st.write(st.session_state["past"])
-# if st.session_state['generated']:
-#     for i in range(0, len(st.session_state["generated"]), 1):
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user', avatar_style='personas')
-#         message(st.session_state['generated'][i], key=str(i), avatar_style='identicon')
-# AvatarStyle = Literal[
-#     "adventurer",
-#     "adventurer-neutral",
-#     "avataaars",
-#     "avataaars-neutral",
-#     "big-ears",
-#     "big-ears-neutral",
-#     "big-smile",
-#     "bottts",
-#     "bottts-neutral",
-#     "croodles",
-#     "croodles-neutral",
-#     "fun-emoji",
-#     "icons",
-#     "identicon",
-#     "initials",
-#     "lorelei",
-#     "lorelei-neutral",
-#     "micah",
-#     "miniavs",
-#     "open-peeps",
-#     "personas",
-#     "pixel-art",
-#     "pixel-art-neutral",
-#     "shapes",
-#     "thumbs",
-# ]
-# chat_placeholder = st.empty()
-
-# with chat_placeholder.container():
-#     for i in range(len(st.session_state['generated'])):                
-#         message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
-#         message(st.session_state['generated'][i], key=f"{i}")
-    
-#     st.button("Clear message", on_click=on_btn_click)
-
-# with st.container():
-#     user_input=st.text_input("User Input:", key='input')
-#     if user_input:
-#         chat_model = create_session()
-#         bot_message = response(chat_model,user_input)
-#         # st.write(bot_message)
-#         output=bot_message
-#         #store the output
-#         st.session_state['past'].append(user_input)
-#         st.session_state['generated'].append(output)
-
-# [
-#             InputOutputTextPair(
-#                 input_text="How many moons does Mars have?",
-#                 output_text="The planet Mars has two moons, Phobos and Deimos.",
-#             ),
-#         ],
+            
